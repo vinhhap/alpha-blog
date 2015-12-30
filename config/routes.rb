a38a7b5d9a4f4fcb010f5d1737b2abec4b80e0f7
do-t <nguyennamquan1996@gmail.com>
2015-12-30 08:42:12 +0000

    Correcct the path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
   
-  root 'welcome#index'
+  get 'about' => 'pages#about'
+  root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
