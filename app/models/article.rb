ae0ff77ba5d8cfbc8b34e019ccbd87641d80db9d
do-t <nguyennamquan1996@gmail.com>
2016-01-13 12:11:57 +0000

    Correct article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ActiveRecord::Base
   has_many :article_categories
   has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence:true, length: { minimum: 10, maximum: 150 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 150 }
   validates :user_id, presence: true
 end
